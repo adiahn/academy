5408277f7e63f5be3300d6e798b6ea16791d3c9f
Adiahn <adnanmukhtar2321@gmail.com>
2025-12-24 00:13:18 +0100

    Add AboutPage route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from './pages/Home'
 import LoginPage from './pages/LoginPage'
 import Dashboard from './pages/Dashboard'
 import InboxPage from './pages/InboxPage'
+import AboutPage from './pages/AboutPage'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/inbox" element={<InboxPage />} />
